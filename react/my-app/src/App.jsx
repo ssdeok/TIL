b9c149589ef72eb090f766e62f67076cfbfc0ebb
ssdeok <bechu123456@gmail.com>
2024-11-06 12:59:49 +0900

    first

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import Profile from './Profile';
-// function Profile() {
-//   return (
-//     <div>
-//       <h1>이름: 정우영</h1>
-//     </div>
-//   );
-// }
-
 function App() {
   return (
     <div>
-      <Profile></Profile>
     </div>
   );
 }
---
+++ react/my-app/src/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Welcome({name}) {
+  return (
+    <div>안녕하세요, {name}님!</div>
+  )
+}
