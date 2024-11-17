9dd281064640cf7aa731594f343ed29b47d18791
ssd <bechu123456@gmail.com>
2024-11-17 21:02:21 +0900

    practice2

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import RedButton from './RedButton';
 // import Counter from './Counter';
 
 export default function App() {
   return (
     <>
-      <div>App</div>
+      <RedButton></RedButton>
+      {/* <div>App</div> */}
       {/* <Counter></Counter> */}
       <Button onClick={() => alert('실행!')} message="haha"></Button>
       <Button onClick={() => alert('취소!')}></Button>
--- react/my-app/src/RedButton.jsx
+++ react/my-app/src/RedButton.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ColorButton from './ColorButton';
 
 export default function RedButton() {
-  return <ColorButton color="red"></ColorButton>;
+  return <ColorButton color="red">레드버튼</ColorButton>;
 }
