78790671414a644c603bed4af75a054f3c4ec5fb
ssdeok <bechu123456@gmail.com>
2024-11-21 20:38:04 +0900

    redux practice

---
+++ react/1121/react_store_1121/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import Hello from "./pages/Hello";
+export default function App() {
+  
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
+}
