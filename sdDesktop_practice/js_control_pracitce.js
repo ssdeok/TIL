d36281cead6293ead83db4a3768536c1de88fd02
ssdeok <bechu123456@gmail.com>
2024-12-04 22:11:57 +0900

    js_ct1

--- sdDesktop_practice/js_control_pracitce.js
+++ sdDesktop_practice/js_control_pracitce.js
@@ -65,8 +65,102 @@ if (isWeekend) {
 
 console.log('-------------------');
 //nums가 다음과 같을 때, 구구단 3단을 출력하시오
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let gugudan of nums) {
-  
+for (let gugudan in nums) {
+  console.log(`3*${nums[gugudan]} = ${3 * nums[gugudan]}`);
+}
+
+console.log('-------------------');
+
+for (let gugu of nums) {
+  console.log(`3* ${gugu} = ${3 * gugu}`);
+}
+
+console.log('-------------------');
+
+// 출석번호를 포함하여 '1번 jun', ..., '4번 lynda'의 형태로 출석을 부르시오.
+const names = ['jun', 'beemo', 'ken', 'lynda'];
+
+for (let check in names) {
+  console.log(`${parseInt(check) + 1}번 ${names[check]}`);
+}
+
+console.log('-------------------');
+let numbers = 1;
+
+for (let naming of names) {
+  console.log(`${numbers}번 ${naming}`);
+  numbers++;
+}
+
+console.log('-------------------');
+
+let index = 0;
+while (index < names.length) {
+  let name = names[index];
+  console.log(index + 1, name);
+  index++;
+}
+
+console.log('-------------------');
+
+const namez = ['jun', 'beemo', 'ken'];
+const ages = [18, 28, 37];
+// 18살 jun
+// 28살 beemo
+// 37살 ken
+// 을 출력하시오.
+
+for (let i = 0; i < namez.length; i++) {
+  console.log(`${ages[i]}살`, namez[i]);
+}
+
+console.log('-------------------');
+
+//주어진 리스트의 길이를 계산하시오
+const namess = ['jun', 'beemo', 'ken', 'lynda'];
+
+console.log(namess.length);
+
+console.log('-------------------');
+
+// 주어진 리스트의 합계를 계산하시오
+const numss = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let two = 0;
+for (let one of numss) {
+  two += one;
+}
+console.log(two);
+
+console.log('-------------------');
+
+// 주어진 리스트에서 최소값을 찾으시오
+const numzz = [10, 6, 8, 5, 4, 2, 3, 11];
+let min = 100;
+for (let num of numzz) {
+  if (num < min) {
+    min = num;
+  }
+}
+console.log(min);
+
+console.log('-------------------');
+
+//주어진 리스트의 순서를 뒤집으시오
+const numm = [10, 6, 8, 5, 4];
+// 결과
+// const reversedNum = [4, 5, 8, 6, 10];
+
+// numm.reverse();
+// console.log(numm);
+
+const reversedNum = [];
+
+for (let index in nums) {
+  let newindex = numm.length - 1 - index;
+
+  let newvalue = nums[newindex];
+
+  reversedNum.push(newvalue);
 }
