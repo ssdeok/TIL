6177f964e849dbf0d37888c63434a28b20f66931
ssd <bechu123456@gmail.com>
2025-01-05 22:53:25 +0900

    java practice sunday

---
+++ java/demo/src/main/java/com/example/demo/JavaPractice.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class JavaPractice {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+        String result = i == j ? "same" : "different";
+
+        System.out.println(result);
+    }
+}
