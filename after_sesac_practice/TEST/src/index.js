ab254a279e61ca3b6497005477470c9d1488d1af
ssdeok <bechu123456@gmail.com>
2025-04-21 19:24:00 +0900

    js object

--- after_sesac_practice/TEST/src/index.js
+++ after_sesac_practice/TEST/src/index.js
@@ -1,8 +1,76 @@
-console.log(num1);
-console.log(num2);
+// let print = function () {
+//   console.log('hello world');
+// };
+// print();
 
-var num1 = 10;
+// const print = () => {
+//   console.log('hello world');
+// };
 
-console.log(num1 + 20);
+// print();
 
-var num2 = 5;
+// function start(name, callback) {
+//   console.log(`안녕하세요 ${name} 입니다. `);
+//   callback();
+// }
+
+// function finish() {
+//   console.log('감사합니다.');
+// }
+
+// start('hyobin', finish);
+
+// let obj = {};
+
+// console.log(obj);
+
+// let book = {
+//   title: '자바스크립트 첫걸음',
+//   author: '서승덕',
+//   category: '자바스크립트',
+//   year: undefined,
+//   color: function () {
+//     console.log('orange');
+//   },
+// };
+
+let car = {
+  name: '붕붕',
+  model: 'morning',
+  color: 'black',
+};
+
+// console.log(car.name);
+// console.log(car.color);
+
+// console.log(car['name']);
+// console.log(car['model']);
+// console.log(car['color']);
+
+// const getValue = (key) => {
+//   console.log(car[key]);
+// };
+
+// getValue('color');
+
+// const cat = {
+//   age: 2,
+//   name: '야옹이',
+//   color: 'white',
+// };
+
+// delete cat.color;
+// delete cat['age'];
+
+// console.log(cat);
+
+// const person = {
+//   name: '홍길동',
+//   age: 20,
+//   print: function () {
+//     console.log(`제 이름은 ${this.name} 입니다.`);
+//   },
+// };
+
+// person.print();
+// person['print']();
