622273fb7ad30d04a5a5a1c7127eafdd08d0ebae
ssdeok <bechu123456@gmail.com>
2025-04-23 19:47:04 +0900

    js 비동기

--- after_sesac_practice/TEST/src/index.js
+++ after_sesac_practice/TEST/src/index.js
@@ -422,3 +422,54 @@
 // print(...numbers);
 
 ///////////////////////////// spread 와 rest
+
+// const workA = () => {
+//   console.log('workA');
+// };
+// const workB = () => {
+//   console.log('workB');
+// };
+// const workC = () => {
+//   console.log('workC');
+// };
+
+// workA();
+// workB();
+// workC();
+
+// const work = (callback) => {
+//   setTimeout(() => {
+//     console.log('비동기');
+//     callback();
+//   }, 3000);
+// };
+
+// work(() => {
+//   console.log('종료');
+// });
+
+const workA = () => {
+  setTimeout(() => {
+    console.log('workA');
+  }, 5000);
+};
+const workB = () => {
+  setTimeout(() => {
+    console.log('workB');
+  }, 3000);
+};
+const workC = () => {
+  setTimeout(() => {
+    console.log('workC');
+  }, 10000);
+};
+const workD = () => {
+  console.log('workD');
+};
+
+workA();
+workB();
+workC();
+workD();
+
+///////////////////////////// 비동기
