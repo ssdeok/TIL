0f0dfe6947711350b77c5c48a9ad95b1382e29de
ssdeok <bechu123456@gmail.com>
2025-04-23 18:18:08 +0900

    js spread and rest

--- after_sesac_practice/TEST/src/index.js
+++ after_sesac_practice/TEST/src/index.js
@@ -361,3 +361,64 @@
 // console.log(c4);
 
 ///////////////////////////// 구조 분해 할당
+
+// spread
+
+// const toy = {
+//   type: 'bear',
+//   price: 15000,
+// };
+
+// const blueToy = {
+//   ...toy,
+//   color: 'blue',
+// };
+
+// const yellowToy = {
+//   ...toy,
+//   color: 'yellow',
+// };
+
+// console.log(blueToy);
+// console.log(yellowToy);
+
+// const color1 = ['red', 'orange', 'yellow'];
+// const color2 = ['blue', 'navy', 'purple'];
+
+// const rainbow = [...color1, 'green', ...color2];
+
+// console.log(rainbow);
+
+// rest
+// 나머지 매개변수
+
+// const blueToy = {
+//   type: 'bear',
+//   price: 15000,
+//   color: 'blue',
+// };
+
+// const { type, ...rest } = blueToy;
+
+// console.log(type);
+// console.log(rest);
+
+// const color = ['red', 'orange', 'yellow', 'green'];
+// const [c1, c2, ...rest] = color;
+// console.log(c1, c2);
+// console.log(rest);
+
+// const print = (a, b, ...rest) => {
+//   console.log([a, b, rest]);
+// };
+
+// print(1, 2, 3, 4, 5, 6);
+
+// const print = (...rest) => {
+//   console.log(rest);
+// };
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// print(...numbers);
+
+///////////////////////////// spread 와 rest
