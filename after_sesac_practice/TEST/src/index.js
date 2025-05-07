7ef8d5a581c406228e201be229d03591719a2711
ssdeok <bechu123456@gmail.com>
2025-05-07 22:18:19 +0900

    js async await Promise.all

--- after_sesac_practice/TEST/src/index.js
+++ after_sesac_practice/TEST/src/index.js
@@ -550,20 +550,53 @@
 // });
 //////////////////////////// 프로미스 객체
 
-const delay = (ms) => {
+// const delay = (ms) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`3초가 지났습니다`);
+//     }, ms);
+//   });
+// };
+
+// const start = async () => {
+//   try {
+//     let result = await delay(3000);
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
+
+const workA = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(`3초가 지났습니다`);
-    }, ms);
+      resolve('workA');
+    }, 5000);
+  });
+};
+const workB = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('workB');
+    }, 3000);
+  });
+};
+const workC = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('workC');
+    }, 10000);
   });
 };
 
 const start = async () => {
   try {
-    let result = await delay(3000);
-    console.log(result);
-  } catch (error) {
-    console.log(error);
+    let results = await Promise.all([workA(), workB(), workC()]);
+    results.forEach((res) => console.log(res));
+  } catch (err) {
+    console.log(err);
   }
 };
 
